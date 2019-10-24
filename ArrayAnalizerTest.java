e882b6c32486841f5ecebbdd403e06d1e9d78bba
DmitrySar <youshin.d@gmail.com>
2019-10-24 12:07:56 +0300

    refactor

--- ArrayAnalizerTest.java
+++ ArrayAnalizerTest.java
@@ -1,8 +1,12 @@
 public class ArrayAnalizerTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IndexOfMaxElementException {
 		ArrayAnalizer analizer = new ArrayAnalizer();
 		int indexOfMaxElement = analizer.getIndexMaxElement(-1, 2, 8, -3, 0);
-		System.out.println("index of max element: " + indexOfMaxElement);
-		System.out.println("Tested is " + (indexOfMaxElement == 2));
+		int rightRes = 2;
+		if (indexOfMaxElement == rightRes) {
+			System.out.println("Tested ok");
+		} else {
+			throw new IndexOfMaxElementException("indexOfMaxElement != " + rightRes);
+		}
 	}
 }
